ae3fe801757090dfaea0251203197b5e72f3b095
yuwoquan <841453858@qq.com>
2017-12-23 09:51:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/viewpagergridview/Subject.java
@@ -0,0 +1,35 @@
+package com.example.viewpagergridview;
+
+/**
+ * Created by CZY on 2017/6/23.
+ */
+public class Subject {
+
+    //主题名
+    private String name;
+
+    //主题图标资源ID
+    private int icon;
+
+    public Subject(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+}
